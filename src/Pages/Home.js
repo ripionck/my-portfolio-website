90fe978259f92e7ee2f411a8fc4ae1b05a64cd76
Your Name <you@example.com>
2022-06-10 19:35:25 +0600

    banner and footer added

---
+++ src/Pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import About from "./About";
+import Banner from "./Banner";
+import Footer from "./Footer";
+
+const Home = () => {
+  return (
+    <div>
+      <Banner></Banner>
+      <About></About>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Home;
