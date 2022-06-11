776039501ca4f164df6f5a258f80562e1675b717
Your Name <you@example.com>
2022-06-11 13:27:50 +0600

    contact email added

--- src/Pages/ContactForm.js
+++ src/Pages/ContactForm.js
@@ -1,27 +1,69 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const ContactForm = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.prevent.default();
+
+    emailjs
+      .sendForm(
+        "service_qfmwtuu",
+        "template_ywq2dlm",
+        form.current,
+        "fmBsYnrMxwWoCsVad"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
-    <div>
-      <div class="drawer">
-        <input id="my-drawer" type="checkbox" class="drawer-toggle" />
-        <div class="drawer-content">
-          {/* <!-- Page content here --> */}
-          <label for="my-drawer" class="btn btn-primary drawer-button">
-            Open drawer
-          </label>
-        </div>
-        <div class="drawer-side">
-          <label for="my-drawer" class="drawer-overlay"></label>
-          <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-            {/* <!-- Sidebar content here --> */}
-            <li>
-              <a>Sidebar Item 1</a>
-            </li>
-            <li>
-              <a>Sidebar Item 2</a>
-            </li>
-          </ul>
+    <div className="hero min-h-screen bg-base-200">
+      <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+        <div className="card-body">
+          <form ref={form} onSubmit={sendEmail}>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Full Name</span>
+              </label>
+              <input
+                type="text"
+                name="name"
+                placeholder="Full Name"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email Address</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Email Address"
+                name="user_email"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Message</span>
+              </label>
+              <textarea
+                className="textarea textarea-bordered h-24"
+                placeholder="Message"
+              ></textarea>
+            </div>
+            <input
+              type="submit"
+              value="Send"
+              className="btn btn-primary w-full mt-4"
+            />
+          </form>
         </div>
       </div>
     </div>
